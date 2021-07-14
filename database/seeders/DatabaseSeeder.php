ff52ab777f00e064e505ab5115e6f50e44b97b2b
AR Shahin <mdshahinmije96@gmail.com>
2021-07-14 20:30:44 +0600

    teacher and student factory:

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\Admin::factory(1)->create();
-
+        \App\Models\Teacher::factory(10)->create();
+        \App\Models\Student::factory(10)->create();
         $this->call([
             Department::class,
             Course::class,
